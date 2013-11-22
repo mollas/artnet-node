16d93a339c2dc428584d2ae83630777f917789b5
SEan <hi.na3s@gmail.com>
2013-11-21 20:25:48 -0800

    looped through and made all pixels in universe white.

--- test/test-client.js
+++ test/test-client.js
@@ -1,6 +1,24 @@
+var total_pixels = 90;
+
 var artnetclient = require('../lib/artnet_client');
 
-var data = [255, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0];
+//data
+var data = new Array();
+for(var i=0;i<total_pixels;i++) {
+	//pixel x
+	for(var c=0;c<3;c++) { //rgb
+		data.push(0); //r,g,b
+	}
+}
 
-var client = artnetclient.createClient('127.0.0.1', 6454);
-client.send(data);
+var controllers = 1;
+
+//universes
+var universes = new Array();
+for(var c=0;c<controllers;c++) {
+	universes.push( artnetclient.createClient('192.168.1.100', 6454, 1) );
+	universes.push( artnetclient.createClient('192.168.1.100', 6454, 2) );
+	universes.push( artnetclient.createClient('192.168.1.100', 6454, 3) );
+	universes.push( artnetclient.createClient('192.168.1.100', 6454, 4) );
+}
+for(var u=0;u<universes.length;u++) universes[u].send(data);
